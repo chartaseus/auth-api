863db135f6f5f312b6eee1015003b72f726dd835
chartaseus <90235896+chartaseus@users.noreply.github.com>
2025-06-03 08:12:39 +0700

    Fix import

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const createServer = require('./Interfaces/http/createServer');
+const createServer = require('./Infrastructures/http/createServer');
 const container = require('./Infrastructures/container');
 
 const start = async () => {
